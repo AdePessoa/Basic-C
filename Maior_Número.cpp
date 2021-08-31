d96f7a65080af7e01f2f35003b8277f0601cb982
AdePessoa <89546177+AdePessoa@users.noreply.github.com>
2021-08-30 23:42:52 -0300

    Add files via upload

---
+++ Maior_Número.cpp
@@ -0,0 +1,37 @@
+/* Determinar o maior entre N números. A condição de parada é a entrada de um valor 0, ou seja, o algoritmo deve
+ficar calculando o maior até que a entrada seja igual a 0 (zero) */
+
+#include <stdio.h>
+
+main ()
+{
+	int valor, valor_maior, count = 0;
+	
+	valor_maior = 0;
+	
+	do
+	{
+		printf("Entre com um valor ou digite 0(zero) para finalizar o programa: ");
+		scanf("%d", &valor);
+		count++;
+		
+		if (valor != 0)
+		{
+			if (valor > valor_maior)
+			{
+				valor_maior = valor;
+			}
+		}
+		else
+		{
+			if (valor_maior == 0 && count ==1)
+			{
+				printf("Nenhum valor foi informado!");
+			}
+			else
+				printf("Maior valor informado entre as %d entradas foi: << %d >>.", count, valor_maior);
+		}
+
+	}	while (valor !=0);	
+	
+}
